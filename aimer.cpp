2fe6fdf8250cf52bd03e7fa51d5dbae14b117962
nhl141 <naihua.lucas.lee@gmail.com>
2025-11-08 08:26:44 -0500

    Initial commit

--- aimer.cpp
+++ aimer.cpp
@@ -1,8 +1,41 @@
-#include <iostream>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <Servo.h>
 
-int main();
-int main()
+const char *ssid = "YourWiFiName";
+const char *password = "YourWiFiPassword";
+
+Servo servo;
+WebServer server(80);
+
+void handleServo()
 {
-    std::cout << "Hi";
-    return 0;
+    if (server.hasArg("angle"))
+    {
+        int angle = server.arg("angle").toInt();
+        servo.write(angle);
+        server.send(200, "text/plain", "Servo moved to " + String(angle));
+    }
+    else
+    {
+        server.send(400, "text/plain", "Missing angle parameter");
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    servo.attach(13); // connect servo signal to pin 13
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+        delay(1000);
+    Serial.println(WiFi.localIP());
+
+    server.on("/servo", handleServo);
+    server.begin();
+}
+
+void loop()
+{
+    server.handleClient();
 }
